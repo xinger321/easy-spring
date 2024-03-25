d6ccd5a6279dabc48240c7b37bce2fb5a9c5e8e7
liuxing <liuxing@juzishuke.com>
2024-03-25 17:28:23 +0800

    first commit

---
+++ src/main/java/com/tgb/esayspring/EsaySpringApplication.java
@@ -0,0 +1,13 @@
+package com.tgb.esayspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsaySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsaySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgb/esayspring/EsaySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgb.esayspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsaySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
